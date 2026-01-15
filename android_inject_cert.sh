873483a1d1c32ceca0150ccae8409ff05039d87f
patarisac <patar.isac@gmail.com>
2026-01-15 20:07:38 +0700

    added support for android 13 or earlier.

--- android_inject_cert.sh
+++ android_inject_cert.sh
@@ -9,10 +9,22 @@ if [ "$CERT_NAME" == "" ]; then
     exit
 fi
 TMP_DIR=/data/local/tmp
-CMD="CERTIFICATE_PATH=$TMP_DIR/$CERT_NAME sh $TMP_DIR/inject.sh"
-
-adb push "$DIR_SCRIPT/inject.sh" $TMP_DIR
 adb push "$DIR_SCRIPT/$CERT_NAME" $TMP_DIR
+
+
+if [ "$(adb shell getprop ro.build.version.sdk | tr -d '\r')" -lt 34 ]; then
+    RES=$(adb shell cp $TMP_DIR/$CERT_NAME /system/etc/security/cacerts 2>&1)
+    echo $RES
+    if [[ "$RES" == *"Read-only file system"* ]]; then
+        echo "Read Only system directory. Add '-writable-system' when running emulator.";
+        echo "Don't forget to 'adb root' and then 'adb remount'."
+    fi
+    echo "Certificate injected."
+    exit 0
+fi
+
+CMD="CERTIFICATE_PATH=$TMP_DIR/$CERT_NAME sh $TMP_DIR/inject.sh"
+adb push "$DIR_SCRIPT/inject.sh" $TMP_DIR
 
 if [ "$(adb shell whoami)" == "root" ]; then
     adb shell "$CMD"
--- der2pem.sh
+++ der2pem.sh
@@ -10,6 +10,7 @@ if [ -f $1 ]; then
     openssl x509 -inform DER -in $1 -out cacert.pem
     openssl x509 -in cacert.pem -text -fingerprint -sha1 -noout >> cacert.pem
     CERT_NAME=$(openssl x509 -inform PEM -subject_hash_old -in cacert.pem | head -1)
+    chmod 644 cacert.pem
     mv cacert.pem "${CERT_NAME}.0"
 else
     echo "$1: No such file."
